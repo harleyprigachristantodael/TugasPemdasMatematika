1888543c11a37aef82abbd697714255f1a454128
harleyprigachristantodael <harleyaprigachristantodael@gmail.com>
2024-11-24 19:35:33 +0700

    Add files via upload

---
+++ Matematika36.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package matematika36;
+ public class Matematika36 {
+   public void pertambahan (int a, int b){
+        int hasil = a + b;
+        System.out.println("Hasil pertambahan : " + hasil);
+    }
+    public void perkurangan(int a, int b){
+        int hasil = a - b;
+        System.out.println("Hasil perkurangan : " + hasil);
+    }
+    public void perkalian(int a, int b){
+        int hasil = a * b;
+        System.out.println("Hasil perkurangan : " + hasil);
+    }
+    public void pembagian(int a, int b){
+        int hasil = a / b;
+        System.out.println("Hasil perkurangan : " + hasil);
+    }
+}
+   
+    
+    
+
---
+++ MatematikaBeraksii.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package matematika36;
+public class MatematikaBeraksii {
+public static void main(String[] args){ 
+    Matematika hasil;
+    hasil = new Matematika ();
+    hasil.pertambahan(0, 0);
+    hasil.perkurangan(0, 0);
+    hasil.perkalian(0, 0);
+    hasil.pembagian(0, 0);
+        
+    }
+    
+}
+
+ 
+    
